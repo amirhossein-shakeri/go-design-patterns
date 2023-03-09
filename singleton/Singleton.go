452427a58175a3c53eacdd7d85a89c11cfc41f21
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-03-09 22:28:35 +0330

    feat: add singleton description comments

--- singleton/Singleton.go
+++ singleton/Singleton.go
@@ -1,5 +1,10 @@
 package singleton
 
+// Singleton: This pattern is used to ensure a class has only one
+// instance, and provides a global point of access to it. In Go,
+// this can be achieved by creating a struct with only private fields,
+// and providing a global function that returns a pointer to the same
+// instance of the struct.
 type Singleton interface {
 	GetInstance() Singleton
 }
